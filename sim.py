ea94f07c11a282e75e9a8b06e3d088293bbf19ab
Joshua Nelson <joshua@nelsn.net>
2013-04-19 11:21:16 -0700

    Adding back a renamed file, sim.py.

---
+++ sim.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+
+class Simulator(object):
+  """
+  Our SM16 Simulator
+  """
+
+  def __init__(self, verbose=False):
+    """
+    Initialize the Simulator and specify verbosity
+    """
+
+    if verbose:
+      self.verbose = True
+    else
+      self.verbose 
