e3ec9e410cf697b00a08b050f9b419f8e241612f
grady.barrett <grady.barrett@gmail.com>
2013-04-26 09:39:05 -0700

    Added user I/O menu

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ imem = Instr_Mem("imem.txt")
 dmem = Data_Mem("dmem.txt")
 prog1 = Simulator(imem.output_array(), dmem.output_array())
 
+while True:
+  print "(1) Run the entire program\n"
+  print "(2) Run a specified number of steps\n"
+  print "(q) Quit"
+
+  userSelection = raw_input()
+  if userSelection == 1
+    prog1.run(self)
+  elif userSelection == 2
+    steps = raw_input("Enter number of steps to run: ")
+    prog1.step(self, steps)
+  elif userSelection == 'q'
+    break
+  else
+    print "Invalid selection"
+
