98b89ed8614344cb528c0566b7ea56bf9ef36839
Joshua Nelson <joshua@nelsn.net>
2013-04-26 09:50:27 -0700

    Progress on read_instruction function.

--- sim.py
+++ sim.py
@@ -30,43 +30,49 @@ class Simulator(object):
     Given an integer representing the number of steps to be taken,
     step forward in the code
     """
+    print self.instruction_mem[self.reg_pc]
+    self.read_instr(self.instruction_mem[self.reg_pc])
 
   def read_values(self):
     """
     Outputs register values
     """
+    print "a: " + str(self.reg_a)
+    print "b: " + str(self.reg_b)
+    print "c: " + str(self.reg_c)
+    print "d: " + str(self.reg_d)
+    print "pc: " + str(self.reg_pc)
 
-  def read_instr(self):
+  def read_instr(self, instr):
     """
     Look at location in imem and determine what we need to do
     Lots of cases (control)
     """
-    for instr in self.instruction_mem:
+    if instr:
+      self.reg_pc += 1
       if instr[0] == "add":
-        vars()["reg_"+instr[1]] = vars()["reg_"+instr[1]] + dmem[instr[2]]
+        setattr(self, 'reg_'+instr[1], getattr(self, 'reg_'+instr[1]) + self.data_mem[instr[2]])
       elif instr[0] == "sub":
-        vars()["reg_"+instr[1]] = vars()["reg_"+instr[1]] - dmem[instr[2]]
-        elif instr[0] == "load":
-            vars()["reg_"+instr[1]] = dmem[instr[2]]
-        elif instr[0] == "store":
-            dmem[instr[2]] = vars()["reg_"+instr[1]]
-        elif instr[0] == "addi":
-            vars()["reg_"+instr[1]] = vars()["reg_"+instr[2]] + instr[3]
-        elif instr[0] == "seti":
-            vars()["reg_"+instr[1]] = instr[2]
-        elif instr[0] == "jump":
-            reg_pc = instr[1]
-        elif instr[0] == "jz":
-            if vars()["reg_"+instr[1]] == None:
-                reg_pc = instr[2]
-            reg_pc += 1
-        elif instr[0] == "addptr":
-            vars()["reg_"+instr[1]] = vars()["reg_"+instr[1]] + dmem[vars()["reg_"+instr[2]]+ instr[3]]
-        elif instr[0] == "subptr":
-            vars()["reg_"+instr[1]] = vars()["reg_"+instr[1]] - dmem[vars()["reg_"+instr[2]]+ instr[3]]
-        elif instr[0] == "loadptr":
-            vars()["reg_"+instr[1]] = dmem[vars()["reg_"+instr[2]]+ instr[3]]
-        elif instr[0] == "storeptr":
-            dmem[vars()["reg_"+instr[2]]+ instr[3]] = vars()["reg_"+instr[1]]
-        else:
-            reg_pc += 1
+        setattr(self, 'reg_'+instr[1], getattr(self, 'reg_'+instr[1]) - self.data_mem[instr[2]])
+      elif instr[0] == "load":
+        setattr(self, 'reg_'+instr[1], self.data_mem[instr[2]])
+      elif instr[0] == "store":
+        setattr(self, data_mem[instr[2]], getattr(self, 'reg_'+instr[1]))
+      elif instr[0] == "addi":
+        setattr(self, 'reg_'+instr[1], getattr(self, 'reg_'+instr[1]) + instr[2])
+      elif instr[0] == "seti":
+        setattr(self, 'reg_'+instr[1], instr[2])
+      elif instr[0] == "jump":
+        self.reg_pc = instr[1]
+      elif instr[0] == "jz":
+        if getattr(self, 'reg_'+instr[1]) is None:
+          self.reg_pc = instr[2]
+      elif instr[0] == "addptr":
+        setattr(self, 'reg_'+instr[1], getattr(self, 'reg_'+instr[1]) + self.data_mem[getattr(self, 'reg_'+instr[2]) + instr[3]])
+      elif instr[0] == "subptr":
+        setattr(self, 'reg_'+instr[1], getattr(self, 'reg_'+instr[1]) - self.data_mem[getattr(self, 'reg_'+instr[2]) + instr[3]])
+      elif instr[0] == "loadptr":
+        setattr(self, 'reg_'+instr[1], self.data_mem[getattr(self, 'reg_'+instr[2])] + instr[3])
+      elif instr[0] == "storeptr":
+        #TODO wasnt sure on this
+        dmem[vars()["reg_"+instr[2]]+ instr[3]] = vars()["reg_"+instr[1]]
